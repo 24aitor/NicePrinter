759239e661550ee7a04392fc1be5f04ac49f7b86
24aitor <contact@aitorriba.com>
2017-06-01 02:32:36 +0200

    Added setup.py and passed Readme to rst

---
+++ README.rst
@@ -0,0 +1,64 @@
+Nice Printer
+============
+
+.. image:: https://img.shields.io/github/release/24aitor/NicePrinter.svg      :target: https://github.com/24aitor/niceprinter/releases
+.. image:: https://img.shields.io/badge/Built%20For-Python%203.X-orange.svg   :target: https://www.python.org
+.. image:: https://img.shields.io/badge/license-MIT-blue.svg   :target: https://raw.githubusercontent.com/24aitor/NicePrinter/master/LICENSE
+
+What is Nice Printer?
+---------------------
+
+Nice printer is a project to improve your python scripts presentations by printing
+nice outputs on your shell easily.
+
+Screenshots of example.py output
+--------------------------------
+
+.. image:: https://i.imgur.com/j1Of5ix.png
+
+.. image:: https://i.imgur.com/uGMzMvK.png
+
+.. image:: https://i.imgur.com/jaLykwa.png
+
+.. image:: https://i.imgur.com/K4N2mK7.png
+
+API
+---
+
+- underline(string)
+
+- darkcyan(string)
+
+- purple(string)
+
+- yellow(string)
+
+- green(string)
+
+- cyan(string)
+
+- blue(string)
+
+- bold(string)
+
+- red(string)
+
+- colorsChars(string)
+
+- center(string)
+
+- hr(percentage = 75, sym = '-')
+
+- box(string, hsym = '-', vsym = '|')
+
+- bbox(string)
+
+- cbox(string, hsym = '-', vsym = '|')
+
+- cbbox(string)
+
+- title(string, percentage = 75, sym = '-')
+
+- listing(head, array, vsym = '|', hsym = '=', centered = False, boldHead = True)
+
+- table(array, vsym = '|', hsym = '=', centered = False, boldHead = True
---
+++ setup.py
@@ -0,0 +1,57 @@
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='Nice Printer',
+
+    # Versions should comply with PEP440.  For a discussion on single-sourcing
+    # the version across setup.py and the project code, see
+    # https://packaging.python.org/en/latest/single_source_version.html
+    version='1.0.0',
+
+    description='A simple Python project to generate nice prints',
+    long_description=long_description,
+
+    # The project's main homepage.
+    url='https://github.com/24aitor/NicePrinter',
+
+    # Author details
+    author='Aitor Riba Archilla',
+    author_email='contact@aitorriba.com',
+
+    # Choose your license
+    license='MIT',
+
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Generate Nice Prints',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: MIT License',
+
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+
+    # What does your project relate to?
+    keywords='print shell',
+)
